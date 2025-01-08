29f2ed98a9d63bbfbe6397426f2829791358053f
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-01-08 10:18:42 -0500

    Add project files.

---
+++ Week1CSharpFundamentals/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Week1CSharpFundamentals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
