af6559c52131c99ad94055a4a9e5113f737d134a
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-01-08 10:33:14 -0500

    literal values

--- Week1CSharpFundamentals/Program.cs
+++ Week1CSharpFundamentals/Program.cs
@@ -13,7 +13,11 @@
                           select n;
 
             // literal values
-
+            int i = 5;
+            double d = 5.5;
+            float f = 5.5F;
+            decimal m = 5.5M;
+            long l = 43543324L;
         }
     }
 }
